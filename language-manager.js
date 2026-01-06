3f5b2231d894996cad6f49c86b94d5ef0f42d341
animofa <moser.anika@yahoo.de>
2026-01-06 11:32:16 +0100

    add romanian to language manger

--- language-manager.js
+++ language-manager.js
@@ -2,6 +2,8 @@ export async function loadVocabularyForLanguage(lang) {
   switch (lang) {
     case "russian":
       return (await import("./russian.js")).default;
+    case "romanian":
+      return (await import("./romanian.js")).default;
     case "portuguese":
       return (await import("./portuguese.js")).default;
     case "english":
