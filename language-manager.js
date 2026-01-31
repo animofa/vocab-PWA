52fc2298367fa536c8245a7fbfa9e5e306edc245
animofa <moser.anika@yahoo.de>
2026-01-31 09:55:50 +0100

    add french trainer

--- language-manager.js
+++ language-manager.js
@@ -6,6 +6,8 @@ export async function loadVocabularyForLanguage(lang) {
       return (await import("./languages/romanian.js")).default;
     case "portuguese":
       return (await import("./languages/portuguese.js")).default;
+    case "french":
+      return (await import("./languages/french.js")).default;
     case "english":
     default:
       return (await import("./languages/english.js")).default;
